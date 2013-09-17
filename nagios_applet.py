2befa388ff6738eb2780b3b14d2df05a65351f61
Volodymyr Kovtun <vova-zms@yandex.ua>
2013-09-17 13:33:24 +0300

    coding ...

--- nagios_applet.py
+++ nagios_applet.py
@@ -1,32 +1,35 @@
 # -*- coding: utf-8 -*-
 
-import sys
-import gobject
+import pygtk
+pygtk.require('2.0')
 import gtk
+import gobject
 import appindicator
 import pynotify
-import os
 import ConfigParser
+import sys
+import os
+from urllib2 import URLError, HTTPError
 from nagios_checker import get_new_nagios_status
 
 
 CHECK_INTERVAL = 60 * 5 * 1000 # mls
 CHECK_INTERVAL = 30 * 1000 # mls
 
-CUSTOM_ICON = 'indicator-messages'
-BAD_AUTH_ICON = 'new-messages-red'
-NAGIOS_MSG_ICON = 'indicator-messages-new'
+NAGIOS_ICON = 'nagios'
+WARNING_ICON = 'warning'
+CRITICAL_ICON = 'critical'
+SETTINGS_ICON = 'settings'
+NETWORK_ICON = 'unknown'
 
-#ICON_PATH = '?'
-#ICON_NAME = '?'
-#def menuitem_response(w, buf, i):
-#    print buf, i
-#    if i == 1:
-#        ind.set_status(appindicator.STATUS_ATTENTION)
-#        ind.set_attention_icon("skype")
-#    elif i == 2:
-#        ind.set_status(appindicator.STATUS_ACTIVE)
-#import pdb; pdb.set_trace()
+SETTINGS_ERR_MSG = {
+    'header': 'CONFIG ERROR',
+    'body': 'please, check config file',
+}
+NETWORK_ERR_MSG = {
+    'header': 'NETWORK ERROR',
+    'body': '',
+}
 
 
 class NagiosApplet(object):
@@ -36,34 +39,38 @@ class NagiosApplet(object):
     def __init__(self):
         pynotify.init("Init")
         self.ind = appindicator.Indicator("nagios-checker",
-            CUSTOM_ICON,
+            NAGIOS_ICON,
             appindicator.CATEGORY_APPLICATION_STATUS)
-        self.ind.set_status(appindicator.STATUS_ACTIVE)
+
+    def run(self):
         self.build_menu()
-        self.nagios_status = {}
+        self.prepare()
+        gtk.main()
+
+    def prepare(self):
+        self.set_icon()
         self.renotify = False
         self.show_disabled = False
         self.auth = {}
+        self.nagios_status = {}
+        self.warning = False
+        self.critical = False
+        self.check_status()
+        self.timeout_id = gobject.timeout_add(CHECK_INTERVAL,
+            self.check_status)
+        
 
     def build_menu(self):
-        """Create applet menu
+        """Create menu
         """
         menu = gtk.Menu()
         item = gtk.MenuItem("Check nagios status now")
         menu.append(item)
         item.connect("activate", self.check_now)
         item.show()
-        #item = gtk.MenuItem("Stop monitoring")
-        #menu.append(item)
-        #item.connect("activate", stop_monitor)
-        #item.show()
-        #item = gtk.MenuItem("Start monitoring")
-        #menu.append(item)
-        #item.connect("activate", start_monitor)
-        #item.show()
         item = gtk.MenuItem("Relaod config")
         menu.append(item)
-        item.connect("activate", self.get_config)
+        item.connect("activate", self.reload_config)
         item.show()
         item = gtk.MenuItem("Quit")
         menu.append(item)
@@ -72,49 +79,49 @@ class NagiosApplet(object):
         self.ind.set_menu(menu)
 
     def check_status(self):
-        # try get auth info in not
-        if not self.auth:
-            self.get_config()
-            self.ind.set_status(appindicator.STATUS_ATTENTION)
-            self.ind.set_attention_icon(BAD_AUTH_ICON)
-        # do job if auth info exists
-        if self.auth:
-            if self.ind.get_attention_icon() == BAD_AUTH_ICON:
-                self.ind.set_status(appindicator.STATUS_ACTIVE)
-            new_nagios_status = get_new_nagios_status(
-                show_disabled=self.show_disabled, **self.auth)
-            for host in new_nagios_status:
-                for service, new_value in new_nagios_status[host].items():
-                    try:
-                        old_value = self.nagios_status[host].pop(service)
-                        if new_value != old_value or self.renotify:
+        if self.check_config():
+            try:
+                new_nagios_status = get_new_nagios_status(
+                    show_disabled=self.show_disabled, **self.auth)
+            except (HTTPError, URLError, RuntimeError): # issue8797
+                self.set_icon(NETWORK_ICON)
+                self.notify(**NETWORK_ERR_MSG)
+                self.auth = {} # try read config again
+            else:
+                self.set_icon() # restore custom icon
+
+                self.warning = False
+                self.critical = False
+
+                for host in new_nagios_status:
+                    for service, new_value in new_nagios_status[host].items():
+                        try:
+                            old_value = self.nagios_status[host].pop(service)
+                            if new_value != old_value or self.renotify:
+                                self.notify(header=host,
+                                    body='{0} {1}'.format(service, new_value))
+                        except KeyError:
                             self.notify(header=host,
                                 body='{0} {1}'.format(service, new_value))
-                    except KeyError:
+                for host in self.nagios_status:
+                    for service in self.nagios_status[host]:
                         self.notify(header=host,
-                            body='{0} {1}'.format(service, new_value))
-            for host in self.nagios_status:
-                for service in self.nagios_status[host]:
-                    self.notify(header=host,
-                        body='{0} {1}'.format(service, 'OK'))
-            self.nagios_status = new_nagios_status
+                            body='{0} {1}'.format(service, 'OK'))
+                self.nagios_status = new_nagios_status
+                self.update_icon()
 
-            if self.nagios_status:
-                self.ind.set_status(appindicator.STATUS_ATTENTION)
-                self.ind.set_attention_icon(NAGIOS_MSG_ICON)
-            else:
-                self.ind.set_status(appindicator.STATUS_ACTIVE)
-        else:
-            self.notify(header='CONFIG ERROR',
-                body='please, check config file')
         return True
 
-    def run(self):
-        self.get_config()
-        self.check_status()
-        self.timeout_id = gobject.timeout_add(CHECK_INTERVAL,
-            self.check_status)
-        gtk.main()
+    def update_icon(self):
+        #update icon after check_status
+        icon = None
+        st = set([v for k in  self.nagios_status.values()
+            for v in  k.values()])
+        if u'CRITICAL' in st:
+            icon = CRITICAL_ICON
+        elif u'WARNING' in st:
+            icon = WARNING_ICON
+        self.set_icon(icon)
 
     def quit(self, menu_item):
         sys.exit(0)
@@ -125,7 +132,7 @@ class NagiosApplet(object):
         self.timeout_id = gobject.timeout_add(CHECK_INTERVAL,
             self.check_status)
 
-    def get_config(self, menu_item=None):
+    def get_config(self):
         self.auth = {}
         home_dir = os.path.expanduser("~")
         conf_file = os.path.join(home_dir, '.nagios_checker')
@@ -146,11 +153,35 @@ class NagiosApplet(object):
                     'passwd': params['passwd'],
                     })
 
+    def check_config(self):
+        if not self.auth:
+            self.get_config()
+        if not self.auth:
+            self.notify(**SETTINGS_ERR_MSG)
+            self.set_icon(SETTINGS_ICON)
+            return False
+        else:
+            return True
+
+    def reload_config(self, menu_item):
+        """ clear all: icon, status etc.
+        start from begin,,, restart check now !
+        """
+        gobject.source_remove(self.timeout_id)
+        self.prepare()
+
+    def set_icon(self, icon=None):
+        if icon:
+            self.ind.set_status(appindicator.STATUS_ATTENTION)
+            self.ind.set_attention_icon(icon)
+        else:
+            self.ind.set_status(appindicator.STATUS_ACTIVE)
+
     def notify(self, header, body=None, type='info'):
         msg = pynotify.Notification(header, body)
-        msg.set_timeout(10)
+        msg.set_timeout(5)
         msg.show()
-        msg.close()
+        #msg.close()
 
 if __name__ == "__main__":
     applet = NagiosApplet()
